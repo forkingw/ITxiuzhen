7a597cf40ac423e24b957b43986b0f274f2e3f32
for <996500646@qq.com>
2020-01-01 14:21:27 +0800

    简单的模态下拉框

--- task5/findNursing.js
+++ task5/findNursing.js
@@ -6,10 +6,16 @@ function seleteFun() {
   var dom = document.getElementsByClassName('options');
   for (var i = 0; i < dom.length; i ++) {
     dom[i].onclick = function () {
-      var op = this.getElementsByClassName('option-content');
-      console.log(op);
+      var op = this.getElementsByClassName('option-content')[0];
+      if ( op.className.indexOf('show')  >= 0 ) {
+        op.className = "option-content"; 
+      }else {
+        op.className = "option-content show"; 
+      }
+      
     }
   }
+
 }
 
 bandEvent();
